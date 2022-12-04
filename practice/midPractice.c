c28f6787e7db291fd675e81158eaab3db8bbb661
Fmajid29 <faizanmajid11@gmail.com>
2022-12-04 23:46:39 +0500

    completed the os practice lab

---
+++ practice/midPractice.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+
+int main(int argc, char* argv[]){
+
+
+    int const SIZE = argc - 1;
+    int arr[SIZE];
+
+    for (int i = 0; i < SIZE; i++){
+        arr[i] = atoi(argv[i + 1]);
+    }
+
+    int pipe1[2];
+    pipe(pipe1);
+
+    write(pipe1[1], arr, sizeof(arr));
+
+    pid_t id = fork();
+
+    if (id == 0){
+        int array[SIZE], temp;
+        read(pipe1[0], array, sizeof(array));
+        
+        for (int i = 0; i < SIZE; i++){
+            temp = array[i];
+            for (int j = i + 1; j < SIZE; j++){
+                if (temp < array[j]){
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        write(pipe1[1], array, sizeof(array));
+    }
+    else{
+        wait(0);
+        pid_t id1;
+        id1 = fork();
+        if (id1 == 0){
+            int array1[SIZE];
+            read(pipe1[0], array1, sizeof(array1));
+            int secMax[1];
+            secMax[0] = array1[1];
+            write(pipe1[1],secMax, sizeof(secMax));
+        }
+        else{
+            wait(0);
+            pid_t id2;
+            id2 = fork();
+            if (id2 == 0){
+                int seM[1];
+                read(pipe1[0], seM, sizeof(seM));
+                printf("%d \n", seM[0]);
+            }
+            else{
+                wait(0);
+            }
+        }
+    }
+
+    return 0;
+}
