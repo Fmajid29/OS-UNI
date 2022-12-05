bf6cf7f08821a60b73663fb288539f2098b59059
Fmajid29 <faizanmajid11@gmail.com>
2022-12-06 01:13:56 +0500

    completed the gl5

--- practice/midPractice.c
+++ practice/midPractice.c
@@ -10,6 +10,7 @@ int main(int argc, char* argv[]){
     int const SIZE = argc - 1;
     int arr[SIZE];
 
+    // recieving data from terminal and putting in integer array
     for (int i = 0; i < SIZE; i++){
         arr[i] = atoi(argv[i + 1]);
     }
@@ -17,11 +18,14 @@ int main(int argc, char* argv[]){
     int pipe1[2];
     pipe(pipe1);
 
+    // writing int array into the pipe.
     write(pipe1[1], arr, sizeof(arr));
 
+    // creating first child
     pid_t id = fork();
 
     if (id == 0){
+        // doing all the functionality of C1
         int array[SIZE], temp;
         read(pipe1[0], array, sizeof(array));
         
@@ -35,24 +39,38 @@ int main(int argc, char* argv[]){
             }
         }
 
+        for (int i = 0; i< SIZE; i++){
+            printf("%d \n", array[i]);
+        }
+
+        // writing decending sorted array back into the pipe
         write(pipe1[1], array, sizeof(array));
     }
     else{
         wait(0);
         pid_t id1;
+        // creating child 2
         id1 = fork();
         if (id1 == 0){
+            // doing functionality of C2
             int array1[SIZE];
+
+            // reading sorted array from the pipe passed by C1
             read(pipe1[0], array1, sizeof(array1));
             int secMax[1];
             secMax[0] = array1[1];
+
+            // writing data back into the pipe.
             write(pipe1[1],secMax, sizeof(secMax));
         }
         else{
             wait(0);
             pid_t id2;
+            // creating child 3
             id2 = fork();
             if (id2 == 0){
+
+                // doing C3 functionality
                 int seM[1];
                 read(pipe1[0], seM, sizeof(seM));
                 printf("%d \n", seM[0]);
